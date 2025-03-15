15c0bd1238210fc5afc0cf346048599374c81c39
Amal43 <amalmoussa757@gmail.com>
2025-03-15 08:26:54 +0200

    serve static files

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Request, Response, NextFunction } from 'express';
 import morgan from 'morgan';
 import tourRouter from './routes/tour.route';
 import userRouter from './routes/user.route';
+import path from 'path';
 
 const app = express();
 
@@ -9,6 +10,7 @@ const app = express();
 app.use(morgan('dev')); //logger
 
 app.use(express.json()); //modify in coming request cause it undefined without this
+app.use(express.static(path.join(__dirname, '../public')));
 app.use((req: Request, res: Response, next: NextFunction) => {
   console.log(`Hello from middleware`);
   next();
