94cd60c786d6afe3ed9822b9064eaef824d96e58
Amal43 <amalmoussa757@gmail.com>
2025-06-26 15:22:38 +0300

    feat(errors): add global uncaughtException and unhandledRejection handlers

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,16 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+/**
+ * Global uncaught exception handler
+ * Catches synchronous errors that bubble up to the event loop without being handled
+ */
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 import app from './app';
 import { connectDB, PORT } from './config/db';
 
@@ -12,6 +22,18 @@ import { connectDB, PORT } from './config/db';
 //4_start server
 
 connectDB();
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`listen to port ${PORT}`);
+});
+
+/**
+ * Global unhandled promise rejection handler
+ * This catches any promise rejections that weren't properly handled in the code
+ */
+process.on('unhandledRejection', (err: any) => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
