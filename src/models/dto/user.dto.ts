cf97972cff7be18e0cb6dd5ba13a6f943f7da092
Amal43 <amalmoussa757@gmail.com>
2025-06-30 23:11:50 +0300

    feat(user) : add user dto

--- src/models/dto/user.dto.ts
+++ src/models/dto/user.dto.ts
@@ -4,44 +4,39 @@ import { Role } from '@prisma/client';
 export class CreateUserDto implements IUser {
   name: string;
   email: string;
-  photo?: string;
+  photo?: string | null;
   role?: Role;
   password: string;
-  passwordConfirm: string;
+  passwordConfirm: string | null | undefined;
   passwordChangedAt?: Date | null;
   passwordResetToken?: string | null;
   passwordResetExpires?: Date | null;
   active?: boolean;
-  createdAt?: Date;
-  updatedAt?: Date;
 
   constructor(bodyRequest: IUser) {
     this.name = bodyRequest.name;
     this.email = bodyRequest.email;
     this.role = bodyRequest.role;
     this.password = bodyRequest.password;
-    this.passwordConfirm = bodyRequest.passwordConfirm;
+    this.passwordConfirm = bodyRequest.passwordConfirm ?? undefined;
     this.passwordChangedAt = bodyRequest.passwordChangedAt;
     this.passwordResetToken = bodyRequest.passwordResetToken;
     this.passwordResetExpires = bodyRequest.passwordResetExpires;
     this.active = bodyRequest.active ?? true;
-    this.createdAt = bodyRequest.createdAt ?? new Date();
-    this.updatedAt = bodyRequest.updatedAt ?? new Date();
   }
 }
 
 export class UpdateUserDto implements Partial<IUser> {
   name?: string;
   email?: string;
-  photo?: string;
+  photo?: string | null;
   role?: Role;
   password?: string;
-  passwordConfirm?: string;
+  passwordConfirm?: string | null;
   passwordChangedAt?: Date | null;
   passwordResetToken?: string | null;
   passwordResetExpires?: Date | null;
   active?: boolean;
-  updatedAt?: Date;
 
   constructor(bodyRequest: Partial<IUser>) {
     Object.assign(this, bodyRequest);
