fe357961ade9a40d40738d0d944e0b653b670441
amalmosai <amalmoussa757@gmail.com>
2025-07-01 23:01:07 +0300

    feat(tours): implement protected tour routes with auth

--- src/routes/tour.route.ts
+++ src/routes/tour.route.ts
@@ -1,6 +1,7 @@
 import express, { NextFunction, Request, Response } from 'express';
 import { TourController } from '../controllers/tour.controller';
 import { TourService } from '../models/dao/tour.dao';
+import { authenticateUser, authorizeRoles } from '../middlewares/auth';
 
 const router = express.Router();
 
@@ -12,26 +13,31 @@ const tourController = new TourController(tourService);
 router
   .route('/top-5-cheap')
   .get(
+    authenticateUser,
     tourController.aliasTopTours.bind(tourController),
     tourController.getAllTours.bind(tourController),
   );
 
 router
   .route('/tour-stats')
-  .get(tourController.getTourStats.bind(tourController));
+  .get(authenticateUser, tourController.getTourStats.bind(tourController));
 
 router
   .route('/monthly-plan/:year')
-  .get(tourController.getMonthlyPlan.bind(tourController));
+  .get(authenticateUser, tourController.getMonthlyPlan.bind(tourController));
 
 router
   .route('/')
-  .get(tourController.getAllTours.bind(tourController))
-  .post(tourController.createTour.bind(tourController));
+  .get(authenticateUser, tourController.getAllTours.bind(tourController))
+  .post(authenticateUser, tourController.createTour.bind(tourController));
 router
   .route('/:id')
-  .get(tourController.getTourById.bind(tourController))
-  .patch(tourController.updateTourById.bind(tourController))
-  .delete(tourController.deleteTourById.bind(tourController));
+  .get(authenticateUser, tourController.getTourById.bind(tourController))
+  .patch(authenticateUser, tourController.updateTourById.bind(tourController))
+  .delete(
+    authenticateUser,
+    authorizeRoles('LEAD_GUIDE', 'ADMIN'),
+    tourController.deleteTourById.bind(tourController),
+  );
 
 export default router;
