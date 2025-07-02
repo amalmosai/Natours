820e3d8207b87ec3348a0b55d2b6a6791bc70187
amalmosai <amalmoussa757@gmail.com>
2025-07-02 12:13:08 +0300

    feat (auth) : update auth middleware

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -2,48 +2,62 @@ import { Request, Response, NextFunction } from 'express';
 import Jwt, { Secret } from 'jsonwebtoken';
 import { createCustomError, HttpCode } from '../utils/apiError';
 import { promisify } from 'util';
+import { UserService } from '../models/dao/user.dao';
+import { asyncWrapper } from '../utils/asynHandler';
 
-export const authenticateUser = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  const authHeader = req.headers.authorization;
-
-  if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    return next(createCustomError(`No token provided`, HttpCode.UNAUTHORIZED));
+declare global {
+  namespace Express {
+    interface Request {
+      user?: any;
+    }
   }
+}
 
-  const token = authHeader.split(' ')[1];
-  if (!token) {
-    return next(
-      createCustomError(
-        `Authorization token not found"`,
-        HttpCode.UNAUTHORIZED,
-      ),
-    );
-  }
+export const authenticateUser = asyncWrapper(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const authHeader = req.headers.authorization;
 
-  try {
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+      return next(
+        createCustomError(`No token provided`, HttpCode.UNAUTHORIZED),
+      );
+    }
+
+    const token = authHeader.split(' ')[1];
+    if (!token) {
+      return next(
+        createCustomError(
+          `Authorization token not found"`,
+          HttpCode.UNAUTHORIZED,
+        ),
+      );
+    }
+
     const decoded = await promisify<string, Secret>(Jwt.verify)(
       token,
       process.env.JWT_SECRET as Secret,
     );
-    req.body.authUser = decoded;
+
+    req.user = decoded;
+    const currentUser = await UserService.getUserById(req.user.id);
+
+    if (!currentUser) {
+      return next(
+        createCustomError(
+          'The user belonging to this token does no longer exist.',
+          HttpCode.UNAUTHORIZED,
+        ),
+      );
+    }
+
+    req.user = currentUser;
     next();
-  } catch (error) {
-    return next(
-      createCustomError(
-        `Not authorized to access this route`,
-        HttpCode.UNAUTHORIZED,
-      ),
-    );
-  }
-};
+  },
+);
 
 export const authorizeRoles = (...roles: string[]) => {
   return (req: Request, res: Response, next: NextFunction) => {
-    if (!roles.includes(req.body.authUser.role)) {
+    if (!roles.includes(req.user.role)) {
       return next(
         createCustomError(
           `Unauthorized to access this route`,
