3f5f0c627ac826802434224d798b4fa804cc51ff
amalmosai <amalmoussa757@gmail.com>
2025-12-14 23:47:52 +0200

    refacor(user): fix update me method

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,43 +1,46 @@
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response } from 'express';
 import { UserService } from '../models/dao/user.dao';
 import { asyncWrapper } from '../utils/asynHandler';
 import { createCustomError, HttpCode } from '../utils/apiError';
-import { updateUserSchema } from '../validations/user.validation'
+import { updateUserSchema } from '../validations/user.validation';
 
-export class UserController{
-   /**
+export class UserController {
+  /**
    *
    * @param userService
    * Dependency injection
    */
-  public constructor(private readonly userService: UserService){}
+  public constructor(private readonly userService: UserService) {}
 
   public updateMe = asyncWrapper(async (req: Request, res: Response) => {
     const { error } = updateUserSchema.validate(req.body);
+
     if (error) {
       throw createCustomError(error.details[0].message, HttpCode.BAD_REQUEST);
     }
 
+    const updatedUser = await this.userService.updateUser(
+      req.user.id,
+      req.body,
+    );
 
     res.status(HttpCode.OK).json({
-    status: 'success',
-    // data: {
-    //   user: updatedUser
-    // },
-    message: 'Profile updated successfully'
+      status: 'success',
+      data: {
+        user: updatedUser,
+      },
+      message: 'Profile updated successfully',
+    });
   });
-  })
 
   public deleteMe = asyncWrapper(async (req: Request, res: Response) => {
     const userId = req.user.id;
     await this.userService.deleteUser(userId);
 
-
     res.status(HttpCode.OK).json({
-        status: 'success',
-        data: null,
-        message: 'Your account has been deleted successfully',
-      });
-  })
-
+      status: 'success',
+      data: null,
+      message: 'Your account has been deleted successfully',
+    });
+  });
 }
